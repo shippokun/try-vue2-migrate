fb4e236150f3930ad6cc7bc5b5f14da4eeb94fe8
shippokun <okashi0tabetai@gmail.com>
2021-05-12 00:59:36 +0900

    chore(main): use composition api plugin

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
+import VueCompositionAPI from "@vue/composition-api";
 import Vue from "vue";
 
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+
+Vue.use(VueCompositionAPI);
 
 Vue.config.productionTip = false;
 
